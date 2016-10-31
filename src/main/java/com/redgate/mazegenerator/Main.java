49114058dd419aee1425c5e49ba28d62f71c404e
Nikos Nakas <nick.nachos86@gmail.com>
2016-10-31 19:50:51 +0200

    Configurable width and height via cli args

--- src/main/java/com/redgate/mazegenerator/Main.java
+++ src/main/java/com/redgate/mazegenerator/Main.java
@@ -1,11 +1,70 @@
 package com.redgate.mazegenerator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class Main {
 
+    private static final String PARAM_WIDTH = "w";
+    private static final String PARAM_HEIGHT = "h";
+    
     public static void main(String[] args) {
-        Maze m = new Maze(25, 40);
+        CommandLine cmd = parseCliArgs(args);
+        int width = getIntArgValue(cmd, PARAM_WIDTH);
+        int height = getIntArgValue(cmd, PARAM_HEIGHT);
+        Maze m = new Maze(width, height);
         m.accept(new DfsMazeVisitor());
         System.out.println(m);
     }
+    
+    private static CommandLine parseCliArgs(String[] args) {
+        Options cliOptions = createCliOptions();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine result = null;
+        
+        try {
+            result = parser.parse(cliOptions, args);
+        }
+        catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+            new HelpFormatter().printHelp("maze-generator", cliOptions);
+            System.exit(1);
+        }
+        
+        return result;
+    }
 
+    private static Options createCliOptions() {
+        Options options = new Options();
+        Option width = new Option(PARAM_WIDTH, "width", true, "the width of the maze");
+        width.setRequired(true);
+        options.addOption(width);
+        Option height = new Option(PARAM_HEIGHT, "height", true, "the height of the maze");
+        height.setRequired(true);
+        options.addOption(height);
+        
+        return options;
+    }
+    
+    private static int getIntArgValue(CommandLine cmd, String argName) {
+        String rawArgValue = cmd.getOptionValue(argName);
+        int result = 0;
+        
+        try {
+            result = Integer.parseInt(rawArgValue);
+        }
+        catch (NumberFormatException ex) {
+            String msg = String.format("Malformed numeric value for arg -%s: %s", argName, rawArgValue);
+            System.out.println(msg);
+            System.exit(1);
+        }
+        
+        return result;
+    }
+    
 }
--- src/main/java/com/redgate/mazegenerator/Maze.java
+++ src/main/java/com/redgate/mazegenerator/Maze.java
@@ -7,30 +7,13 @@ public class Maze {
     private Cell[][] cells;
     
     public Maze(int width, int height) {
+        this.validateDimention("width", width);
+        this.validateDimention("height", height);
+        
         this.width = width;
         this.height = height;
         this.cells = new Cell[height][width];
-        
-        for (int row = 0; row < height; row++) {
-            for (int column = 0; column < width; column++) {
-                Cell currentCell = new Cell();
-                this.cells[row][column] = currentCell;
-                
-                if (row > 0) {
-                    Wall w = new Wall();
-                    Cell top = this.cells[row - 1][column];
-                    currentCell.setTop(new Joint(top, w));
-                    top.setBottom(new Joint(currentCell, w));
-                }
-                
-                if (column > 0) {
-                    Wall w = new Wall();
-                    Cell left = this.cells[row][column - 1];
-                    currentCell.setLeft(new Joint(left, w));
-                    left.setRight(new Joint(currentCell, w));
-                }
-            }
-        }
+        this.buildMaze();
     }
     
     public void accept(MazeVisitor visitor) {
@@ -69,6 +52,36 @@ public class Maze {
         }
         
         return sb.toString();
+    }
+    
+    private void validateDimention(String name, int value) {
+        if (value < 1) {
+            String msg = String.format("Illegal %s: %d. Maze dimentions can only be positive integers.", name, value);
+            throw new IllegalArgumentException(msg);
+        }
+    }
+    
+    private void buildMaze() {
+        for (int row = 0; row < this.height; row++) {
+            for (int column = 0; column < this.width; column++) {
+                Cell currentCell = new Cell();
+                this.cells[row][column] = currentCell;
+                
+                if (row > 0) {
+                    Wall w = new Wall();
+                    Cell top = this.cells[row - 1][column];
+                    currentCell.setTop(new Joint(top, w));
+                    top.setBottom(new Joint(currentCell, w));
+                }
+                
+                if (column > 0) {
+                    Wall w = new Wall();
+                    Cell left = this.cells[row][column - 1];
+                    currentCell.setLeft(new Joint(left, w));
+                    left.setRight(new Joint(currentCell, w));
+                }
+            }
+        }
     }
     
 }
